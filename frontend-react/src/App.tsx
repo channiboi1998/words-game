0ff063a9a7326051b5d26eff1f9496be9f98a166
channiboi1998 <christian.e.verzosa@gmail.com>
2022-10-28 10:20:59 +0800

    Initial project setup moved to repository

---
+++ frontend-react/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App">
+            Hello this is the frontend
+        </div>
+    );
+}
+
+export default App;
